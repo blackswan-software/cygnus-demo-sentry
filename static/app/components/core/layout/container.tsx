58bd804cb3f0288995d1cba789d37ee45628112a
Nate Moore <nate.moore@sentry.io>
2026-04-01 12:43:18 -0400

    feat(scraps): use opinionated scrollbar styles

--- static/app/components/core/layout/container.tsx
+++ static/app/components/core/layout/container.tsx
@@ -1,5 +1,6 @@
 import type React from 'react';
 import isPropValid from '@emotion/is-prop-valid';
+import {css} from '@emotion/react';
 import styled from '@emotion/styled';
 
 import type {
@@ -7,6 +8,7 @@ import type {
   RadiusSize,
   SpaceSize,
   SurfaceVariant,
+  Theme,
 } from 'sentry/utils/theme';
 
 import {
@@ -266,10 +268,7 @@ export const Container = styled(
   ${p => rc('left', p.left, p.theme)};
   ${p => rc('right', p.right, p.theme)};
 
-  ${p => rc('overflow', p.overflow, p.theme)};
-  ${p => rc('overflow-x', p.overflowX, p.theme)};
-  ${p => rc('overflow-y', p.overflowY, p.theme)};
-
+  ${p => getOverflowStyle(p)};
   ${p => rc('overscroll-behavior', p.overscrollBehavior, p.theme)};
 
   ${p => rc('pointer-events', p.pointerEvents, p.theme)};
@@ -333,3 +332,41 @@ export const Container = styled(
 ` as unknown as <T extends ContainerElement = 'div'>(
   props: ContainerProps<T> | ContainerPropsWithRenderFunction<T>
 ) => React.ReactElement;
+
+function getOverflowStyle(
+  props: Pick<ContainerLayoutProps, 'overflow' | 'overflowX' | 'overflowY'> & {
+    theme: Theme;
+  }
+) {
+  const hasScrollableOverflow = checkOverflowCanScroll(props);
+  return css`
+    ${rc('overflow', props.overflow, props.theme)};
+    ${rc('overflow-x', props.overflowX, props.theme)};
+    ${rc('overflow-y', props.overflowY, props.theme)};
+
+    ${hasScrollableOverflow
+      ? css`
+          scrollbar-width: thin;
+          scrollbar-color: ${props.theme.tokens.graphics.neutral.muted} transparent;
+        `
+      : undefined}
+  `;
+}
+
+function checkOverflowCanScroll(
+  props: Pick<ContainerLayoutProps, 'overflow' | 'overflowX' | 'overflowY'>
+) {
+  for (const property of ['overflow', 'overflowX', 'overflowY'] as const) {
+    const value = props[property];
+    if (!value) {
+      return false;
+    }
+    if (typeof value === 'string') {
+      return value === 'auto' || value === 'scroll';
+    }
+    for (const v of Object.values(value)) {
+      return v === 'auto' || v === 'scroll';
+    }
+  }
+  return false;
+}
