775526892d002406b1636888f1f0c49cfa1e596d
Josh Callender <1569818+saponifi3d@users.noreply.github.com>
2026-04-07 20:06:11 -0700

    WIP

--- static/app/views/detectors/components/forms/common/environmentField.tsx
+++ static/app/views/detectors/components/forms/common/environmentField.tsx
@@ -1,16 +1,18 @@
-import styled from '@emotion/styled';
+import styled from "@emotion/styled";
 
-import {SelectField} from 'sentry/components/forms/fields/selectField';
-import {useFormField} from 'sentry/components/workflowEngine/form/useFormField';
-import {t} from 'sentry/locale';
-import {useProjects} from 'sentry/utils/useProjects';
+import { SelectField } from "sentry/components/forms/fields/selectField";
+import { useFormField } from "sentry/components/workflowEngine/form/useFormField";
+import { t } from "sentry/locale";
+import { useProjects } from "sentry/utils/useProjects";
 
 export interface EnvironmentConfig {
   fieldProps?: Partial<React.ComponentProps<typeof SelectField>>;
   includeAllEnvironments?: boolean;
 }
 
-type EnvironmentFieldProps = Partial<React.ComponentProps<typeof SelectField>> & {
+type EnvironmentFieldProps = Partial<
+  React.ComponentProps<typeof SelectField>
+> & {
   includeAllEnvironments?: boolean;
 };
 
@@ -18,25 +20,34 @@ export function EnvironmentField({
   includeAllEnvironments = true,
   ...props
 }: EnvironmentFieldProps) {
-  const {projects} = useProjects();
-  const projectId = useFormField<string>('projectId')!;
+  const { projects } = useProjects();
+  const projectId = useFormField<string>("projectId")!;
 
-  const environments = projects.find(p => p.id === projectId)?.environments ?? [];
+  const environments =
+    projects.find((p) => p.id === projectId)?.environments ?? [];
 
   return (
     <StyledEnvironmentField
       choices={[
-        ...(includeAllEnvironments ? [['', t('All Environments')] as const] : []),
-        ...(environments?.map(environment => [environment, environment] as const) ?? []),
+        ...(includeAllEnvironments
+          ? [["", t("All Environments")] as const]
+          : []),
+        ...(environments?.map(
+          (environment) => [environment, environment] as const
+        ) ?? []),
       ]}
       inline={false}
       flexibleControlStateSize
       stacked
       name="environment"
-      label={t('Environment')}
-      placeholder={t('Environment')}
-      aria-label={t('Select Environment')}
+      label={t("Environment")}
+      placeholder={t("Environment")}
+      aria-label={t("Select Environment")}
       size="sm"
+      creatable
+      onCreateOption={(env) => {
+        console.log("yeeet!", env);
+      }}
       {...props}
     />
   );
